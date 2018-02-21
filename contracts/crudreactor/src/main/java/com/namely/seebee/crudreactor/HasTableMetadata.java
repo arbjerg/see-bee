2e9b8b56f0b2f0e13451f706d9272b94b652e36e
Dan Lawesson <dan@speedment.com>
2018-02-21 12:24:02 -0800

    crudreactor: Add table schema to metadata

--- contracts/crudreactor/src/main/java/com/namely/seebee/crudreactor/HasTableMetadata.java
+++ contracts/crudreactor/src/main/java/com/namely/seebee/crudreactor/HasTableMetadata.java
@@ -4,11 +4,25 @@ import java.util.Collection;
 
 public interface HasTableMetadata {
     /**
+     * Returns the name of the schema of the table
+     * @return the name of the schema of the table
+     */
+    String schemaName();
+
+    /**
      * Returns the name of the table
      *
      * @return the name of the table
      */
     String tableName();
+
+    /**
+     * Returns the qualified name of the table
+     * @return the qualified name of the table
+     */
+    default String qualifiedName() {
+        return schemaName() + '.' + tableName();
+    }
 
     /**
      * Returns the metadata of the columns. The order may or may not be significant.
