e9e846f3e98bf08c49a8c938c171527a4e98fa45
Dan Lawesson <dan@speedment.com>
2018-02-23 01:03:29 -0800

    Add license headers

--- integration-test/sql-server/tester/src/test/java/com/namely/seebee/test/tester/TestTypes.java
+++ integration-test/sql-server/tester/src/test/java/com/namely/seebee/test/tester/TestTypes.java
@@ -2,7 +2,7 @@
  *
  * Copyright (c) 2017-2018, Namely, Inc. All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); You may
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at:
  *
